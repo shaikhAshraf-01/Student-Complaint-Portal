ddd458b96a937e67a5d06c7b0877d7392afc9870
Ashraf Shaikh <ashrafshaikh5683@gmail.com>
2026-07-22 18:35:47 +0530

    conflict solve

--- Frontend/src/components/adminComponents/dashboardComponent/RecentComplaints.jsx
+++
@@ -1,9 +0,0 @@
-function RecentComplaint(){
-    return (
-        <div>
-            <h1>Old Pending Complaints</h1>
-            <p>Complainst not yet solved</p>
-        </div>
-    )
-}
-export default RecentComplaint;
